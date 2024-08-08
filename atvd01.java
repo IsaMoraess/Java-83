c8f1fd5dae7fca9d23b1bf4bcf9020cf0a9d3018
IsaMoraess <135661414+IsaMoraess@users.noreply.github.com>
2024-08-08 19:42:49 -0300

    Add files via upload

---
+++ atvd01.java
@@ -0,0 +1,9 @@
+/*1) Mostre os números 5 a 50, variando de 5 em 5
+ Exemplo: 5 10 15 ... 50 */
+public class atvd01 {
+    public static void main (String[] args) throws Exception {
+        for (int i = 5; i <= 50; i+=5){ //
+            System.out.println(i);
+        }
+    }
+}
---
+++ atvd01While.java
@@ -0,0 +1,11 @@
+/*1) Mostre os números 5 a 50, variando de 5 em 5
+ Exemplo: 5 10 15 ... 50 */
+public class atvd01While {
+    public static void main (String[] args) throws Exception {
+        int i = 5;
+        while(i <=50){
+            System.out.println(i);
+            i += 5;
+        }
+}
+}
